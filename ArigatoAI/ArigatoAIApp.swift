b8b15c92a2aacdb3255b0f0ea2605c181fbe31e0
OnigiriMerchant <josedaniel.castell@gmail.com>
2026-05-06 23:18:31 +0900

    chore: initial Xcode project scaffold
    - SwiftUI iOS app, deployment target iOS 26
    - SwiftData enabled for transcript persistence
    - Swift Testing framework
    - Bundle ID: com.jose.ArigatoAI
    
    Generated by Xcode 26.4 New Project wizard.

---
+++ ArigatoAI/ArigatoAIApp.swift
@@ -0,0 +1,32 @@
+//
+//  ArigatoAIApp.swift
+//  ArigatoAI
+//
+//  Created by Jose Castell on 2026/05/06.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ArigatoAIApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
